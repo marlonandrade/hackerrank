27cfb9af4e6852a12593fd19b68abab0fcd307c0
Marlon Andrade <marlonmandrade@gmail.com>
2015-09-01 10:05:10 -0300

    removing unused code

--- algorithms/strings/funny-string/script.rb
+++ algorithms/strings/funny-string/script.rb
@@ -9,12 +9,6 @@ class String
     end
   end
 end
-def exactly_divide(n)
-  digits = n.to_s.split('').map &:to_i
-  digits.select do |digit|
-    n % digit == 0 if digit > 0
-  end.count
-end
 
 test_cases = gets.to_i
 test_cases.times do
